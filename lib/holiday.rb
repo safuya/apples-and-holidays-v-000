9274c9bfc858db9205ce6e36098b8d723e54341a
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-23 11:32:03 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |holiday, details|
-    string = "#{holiday:}".slice(0, 1).capitalize + "#{holiday:}".slice(1..-1)
+    string = "#{holiday}:".slice(0, 1).capitalize + "#{holiday}:".slice(1..-1)
     puts string
     holiday.each do |festival, supplies|
       print "#{festival}: "
